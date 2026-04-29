d7996b9dafa14df6e6b3952b24baa87f5350a93b
GalOzRlz <gal.y.oz@proton.me>
2026-04-29 14:05:16 +0300

    first!
    instruments

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::sync::{Arc, Mutex};
+mod sounds;
+
+use crossbeam_queue::SegQueue;
+use crossbeam_utils::atomic::AtomicCell;
+use midi_fundsp::{
+    io::{
+        Speaker, SynthMsg, get_first_midi_device, console_choice_from, start_input_thread,
+        start_output_thread,
+    },
+    sound_builders::ProgramTable,
+};
+use midir::MidiInput;
+
+fn main() -> anyhow::Result<()> {
+    let reset = Arc::new(AtomicCell::new(false));
+    let mut quit = false;
+    while !quit {
+        let mut midi_in = MidiInput::new("midir reading input")?;
+        let in_port = get_first_midi_device(&mut midi_in)?;
+        let midi_msgs = Arc::new(SegQueue::new());
+        let inputs: Arc<SegQueue<SynthMsg>> = Arc::new(SegQueue::new());
+        let outputs = Arc::new(SegQueue::new());
+        while reset.load() {}
+        start_input_thread(midi_msgs.clone(), midi_in, in_port, reset.clone());
+        let program_table = Arc::new(Mutex::new(sounds::favorites()));
+        start_output_thread::<6>(midi_msgs.clone(), program_table.clone());
+        run_chooser(midi_msgs, program_table.clone(), reset.clone(), &mut quit);
+        std::thread::spawn(move || {
+            loop {
+                if let Some(msg) = inputs.pop() {
+                    if let Some((note, velocity)) = msg.note_velocity() {
+                        println!("note: {note} velocity: {velocity}");
+                    }
+                    outputs.push(msg);
+                }
+            }
+        });
+    }
+    Ok(())
+}
+
+fn run_chooser(
+    midi_msgs: Arc<SegQueue<SynthMsg>>,
+    program_table: Arc<Mutex<ProgramTable>>,
+    reset: Arc<AtomicCell<bool>>,
+    quit: &mut bool,
+) {
+    let main_menu = vec!["Pick New Synthesizer Sound", "Pick New MIDI Device", "Quit"];
+    while !*quit && !reset.load() {
+        println!("Play notes at will. When ready for a change, select one of the following:");
+        match console_choice_from("Choice", &main_menu, |s| *s) {
+            0 => {
+                let program = {
+                    let program_table = program_table.lock().unwrap();
+                    console_choice_from("Change synth to", &program_table, |opt| opt.0.as_str())
+                };
+                midi_msgs.push(SynthMsg::program_change(program as u8, Speaker::Both));
+            }
+            1 => reset.store(true),
+            2 => *quit = true,
+            _ => panic!("This should never happen."),
+        }
+    }
+}
